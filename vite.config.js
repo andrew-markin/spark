1288c3f97362bd6beff29e6fa762f2824cee0b30
Andrew N. Markin <andrew.n.markin@gmail.com>
2025-02-17 00:05:13 +0300

    PWA support introduced

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,44 @@ import { fileURLToPath, URL } from 'node:url'
 import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue({
       template: { transformAssetUrls }
+    }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'inline',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,png,svg,ttf,woff,woff2}']
+      },
+      manifest: {
+        name: 'Spark',
+        short_name: 'Spark',
+        description:
+          'Lightweight brain training app designed to boost your memory and cognitive skills',
+        theme_color: '#306ca1',
+        background_color: '#306ca1',
+        display: 'standalone',
+        orientation: 'portrait-primary',
+        icons: [
+          {
+            src: 'favicon.svg',
+            sizes: 'any',
+            type: 'image/svg+xml',
+            purpose: 'any'
+          },
+          {
+            src: 'favicon.png',
+            sizes: '180x180',
+            type: 'image/png',
+            purpose: 'any'
+          }
+        ]
+      }
     }),
     // @quasar/plugin-vite options list:
     // https://github.com/quasarframework/quasar/blob/dev/vite-plugin/index.d.ts
