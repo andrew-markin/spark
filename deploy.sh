377be66343d743f0ae89d7b9d35d08001ce10973
Andrew N. Markin <andrew.n.markin@gmail.com>
2025-08-15 16:22:43 +0300

    Minor fixes

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ rm -rf dist || true
 npm run build
 cd dist
 
-git init
+git init -b main
 git add -A
 git commit -m 'Build deployed'
 git push -f git@github.com:andrew-markin/spark.git main:gh-pages
